134548e091458ef8f9d9b658643da9037e116d2f
牛 青霞 <niuqingxia@qilingu.com>
2025-06-18 16:20:25 +0800

    feat: 加上噪声算法的随机山脉地形

--- html-demo/mesh5.js
+++ html-demo/mesh5.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-
+import {createNoise2D} from 'simplex-noise'
 
 
 
@@ -8,10 +8,15 @@ import * as THREE from 'three';
 //2. 分段为100看效果
 const geometry=new THREE.PlaneGeometry(300,300,100,100);
 
+const noise2D=createNoise2D();
 const positions=geometry.attributes.position;
 
 for(let i=0;i<positions.count;i++){
-    positions.setZ(i,Math.random()*50)
+    const x=positions.getX(i);
+    const y=positions.getY(i);
+    // const z=noise2D(x/100,y/100)*50;
+    const z=noise2D(x/300,y/300)*50;
+    positions.setZ(i,z)
 }
 const material=new THREE.MeshBasicMaterial({
     color:new THREE.Color('orange'),
