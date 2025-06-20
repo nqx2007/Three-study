8fad49097f2b3b23f123ee75d7887a46de648f59
牛 青霞 <niuqingxia@qilingu.com>
2025-06-20 11:42:08 +0800

    feat: 基础画个房子

---
+++ html-demo/house/house.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three'
+import foundation from './foundation.js'
+import sideWall from './side-wall.js'
+import behindWall from './behind-wall.js'
+import frontWall from './front-wall.js'
+import roof from './roof.js'
+import doorstep from './door-step.js'
+
+
+const house=new THREE.Group();
+const sideWall2 = sideWall.clone();
+const roof2 = roof.clone();
+
+
+sideWall.rotateY(Math.PI / 2);
+sideWall.translateZ(-2000);
+sideWall.translateX(1500);
+sideWall.translateY(150);
+
+
+sideWall2.rotateY(Math.PI / 2);
+sideWall2.translateZ(1900);
+sideWall2.translateX(1500);
+sideWall2.translateY(150);
+roof2.rotateX( 70 / 180 * Math.PI);
+roof2.position.z = -roof.position.z;
+
+house.add(foundation)
+house.add(sideWall)
+house.add(sideWall2)
+house.add(behindWall)
+house.add(frontWall)
+house.add(roof)
+house.add(roof2)
+house.add(doorstep)
+
+
+
+export default house
--- html-demo/index.js
+++ html-demo/index.js
@@ -20,18 +20,19 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls'
 // import mesh from './mesh15.js'  // 曲线
 // import mesh from './mesh16.js'  // 管道
 // import mesh from './mesh17.js'  // 形状
-import mesh,{tubePoints} from './mesh18.js'  // 管道穿梭
+// import mesh,{tubePoints} from './mesh18.js'  // 管道穿梭
+import house from './house/house.js'
 const scene = new THREE.Scene()
 
-scene.add(mesh)
+scene.add(house)
 
-const pointLight=new THREE.PointLight(0xffffff,10000)
-pointLight.position.set(80,80,80)
-scene.add(pointLight)
+// const pointLight=new THREE.PointLight(0xffffff,10000)
+// pointLight.position.set(80,80,80)
+// scene.add(pointLight)
 
-// const directionLight=new THREE.DirectionalLight(0xffffff)
-// directionLight.position.set(100,100,100)
-// scene.add(directionLight)
+const directionLight=new THREE.DirectionalLight(0xffffff)
+directionLight.position.set(100,100,100)
+scene.add(directionLight)
 
 
 const ambientLight=new THREE.AmbientLight(0x404040)
@@ -43,23 +44,16 @@ scene.add(ambientLight)
 
 const width= window.innerWidth;
 const height=window.innerHeight;
-const camera=new THREE.PerspectiveCamera(60,width/height,1,10000)
-camera.position.set(200,200,200)
+const camera=new THREE.PerspectiveCamera(60,width/height,1,30000)
+camera.position.set(5000,5000,5000)
 camera.lookAt(0,0,0)
 
 
 const renderer=new THREE.WebGLRenderer()
 renderer.setSize(width,height)
 
-let i=0
+
 function render(){
-    if(i< tubePoints.length - 1) {
-        camera.position.copy(tubePoints[i]);
-        camera.lookAt(tubePoints[i + 1]);
-        i += 1;
-    } else {
-        i = 0;
-    }
     renderer.render(scene,camera)
     requestAnimationFrame(render)
 }
